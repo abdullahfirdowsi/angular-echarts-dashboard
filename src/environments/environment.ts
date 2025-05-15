b28c49356e96ce666a6dfe95baacefe3d78883ab
Abdullah Firdowsi <abdullahtrickings@gmail.com>
2025-05-15 12:08:38 +0530

    Update environment.ts: live url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // filepath: c:\Data\Works\DEX\Angular\angular-echarts-dashboard\src\environments\environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5275/api'
+    apiUrl: 'https://interns-api-ovvy.onrender.com/api'
   };
